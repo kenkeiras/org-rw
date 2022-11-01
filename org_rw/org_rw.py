d1d5de89b7c808f9d6c1cac6526b7a201d4bd0b7
Sergio Martínez Portela <sergio@codigoparallevar.com>
2022-11-01 19:13:29 +0100

    Revert "Fix parsing of lists inside drawers."
    This reverts commit 5e4a9f8ff2df3deac85b6fd4d08cadf4be6361e3.
    
    The tree should not be modified, as it's the returned value. We need an
         additional tree for parent-child element connections.

--- org_rw/org_rw.py
+++ org_rw/org_rw.py
@@ -489,26 +489,15 @@ class Headline:
                 elif content.strip().upper() == ":END:":
                     if current_node is None:
                         logging.warning('Finished node (:END:) with no known starter')
-                    else:
-                        tree_up = list(tree)
-                        while len(tree_up) > 0:
-                            node = tree_up[-1]
-                            if (isinstance(
-                                    node, dom.PropertyDrawerNode
-                            ) or isinstance(
-                                node, dom.LogbookDrawerNode
-                            ) or isinstance(
-                                node, dom.ResultsDrawerNode
-                            )):
-                                tree = tree_up
-                                current_node = node
-                                break
-                            else:
-                                tree_up.pop(-1)
-                        else:
-                            raise Exception('Unexpected node ({}) on headline (id={}), line {}'.format(current_node, self.id, linenum))
-                        current_node = None
-                    tree.pop()
+                    elif not (isinstance(
+                        current_node, dom.PropertyDrawerNode
+                    ) or isinstance(
+                        current_node, dom.LogbookDrawerNode
+                    ) or isinstance(
+                        current_node, dom.ResultsDrawerNode
+                    )):
+                        raise Exception('Unexpected node: {}'.format(current_node))
+                    current_node = None
                 elif content.strip().upper() == ":RESULTS:":
                     assert current_node is None
                     current_node = dom.ResultsDrawerNode()
